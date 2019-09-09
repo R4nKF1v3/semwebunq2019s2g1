e2f7e0f4b92a97dd830f43507e49b4bcb0cbd419
EspinolaAbel <espinola.abel.e@gmail.com>
2019-09-09 03:41:48 -0300

    Se implementa agregar y borrar artistas, albumes y tracks. Pendiente borrar tracks de playlists (punto 2). Pendiente validar tests

--- visado_1/ts/unqfy.ts
+++ visado_1/ts/unqfy.ts
@@ -8,10 +8,29 @@ class UNQfy {
   //TODO: Preguntar kiesesto
   private listeners = [];
 
-  private idCounter: number;
+  private idCounter: any = {
+    artistId: 0,
+    albumId: 0,
+    trackId: 0,
+  };
 
-  private tracks: Array<Track>;
-  private albums: Array<Album>;
+  // AE: lo comente porque según el diagrama UML los tracks están dentro de los albums y los albums están dentro de los artistas
+  //     los voy a reemplazar por metodos.
+  //private tracks: Array<Track>;
+  //private albums: Array<Album>;
+
+  private allTracks(): Array<Track> {
+    return this.allAlbums().reduce( (acum, album) => 
+        acum.concat(album.tracks), []
+    );
+  }
+
+  private allAlbums(): Array<Album> {
+    return this.artists.reduce( (acum, artist) => 
+        acum.concat(artist.albums), []
+    );
+  }
+
   private artists: Array<Artist>;
   private playlists: Array<Playlist>;
   private users: Array<User>;
@@ -21,33 +40,59 @@ class UNQfy {
     this.artists = [];
   }
 
+  getNewArtistId(): number {
+    this.idCounter.artistId++;
+    return this.idCounter.artistId;
+  }
+
+  getNewAlbumId(): number {
+    this.idCounter.albumId++;
+    return this.idCounter.albumId;
+  }
+  
+  getNewTrackId(): number {
+    this.idCounter.trackId++;
+    return this.idCounter.trackId;
+  }
+
   //Switch que ejecuta los comandos dependiendo del término
   executeWith(command: string, args: Array<string>){
     switch (command) {
       case "addArtist":
-        this.addArtist({name: args[0], country: args[1]})
+        return this.addArtist({name: args[0], country: args[1]})
       case "getArtist":
         return this.getArtistById(parseInt(args[0]))
+      case "addAlbum":
+        return this.addAlbum(parseInt(args[0]), {name: args[1], year: args[2]});
+      case "addTrack":
+        const GENRES_ARRAY = JSON.parse(args[3]);
+        return this.addTrack(parseInt(args[0]), {name: args[1], duration: args[2], genres: GENRES_ARRAY});
+      case "deleteArtist":
+        return this.deleteArtist(parseInt(args[0]));
+      case "deleteAlbum":
+        return this.deleteAlbum(parseInt(args[0]));
+      case "deleteTrack":
+        return this.deleteTrack(parseInt(args[0]));
+      default:
+        throw new Error(`El comando '${command}' no es un comando válido`);
     }
-  }
-
-  getNewId(): number{
-    const ret = this.idCounter;
-    this.idCounter++;
-    return ret;
   }
 
   // artistData: objeto JS con los datos necesarios para crear un artista
   //   artistData.name (string)
   //   artistData.country (string)
   // retorna: el nuevo artista creado
-  addArtist(artistData) {
+  addArtist(artistData: any) {
   /* Crea un artista y lo agrega a unqfy.
   El objeto artista creado debe soportar (al menos):
     - una propiedad name (string)
     - una propiedad country (string)
   */
-    this.artists.push(new Artist(this.getNewId(), artistData.name, artistData.country))
+    this.artists.push(new Artist(this.getNewArtistId(), artistData.name, artistData.country))
+  }
+
+  deleteArtist(artistId: number) {
+    this.artists = this.artists.filter( artist => artist.id !== artistId);
   }
 
 
@@ -55,59 +100,89 @@ class UNQfy {
   //   albumData.name (string)
   //   albumData.year (number)
   // retorna: el nuevo album creado
-  addAlbum(artistId : number, albumData) {
+  addAlbum(artistId : number, albumData: any) {
   /* Crea un album y lo agrega al artista con id artistId.
     El objeto album creado debe tener (al menos):
      - una propiedad name (string)
      - una propiedad year (number)
   */
+    let artist = this.getArtistById(artistId);
+
+    if (artist == null)
+      throw new Error(`No se pudo encontrar el artista con id ${artistId}`);
+
+    const newAlbum = new Album(this.getNewAlbumId(), albumData.name, albumData.year);
+    artist.addAlbum(newAlbum);
+    return newAlbum;
   }
 
+  deleteAlbum(albumId: number) {
+    this.artists.forEach( artist => 
+      artist.albums = artist.albums.filter( album => album.id !== albumId )
+    )
+  }
 
   // trackData: objeto JS con los datos necesarios para crear un track
   //   trackData.name (string)
   //   trackData.duration (number)
   //   trackData.genres (lista de strings)
   // retorna: el nuevo track creado
-  addTrack(albumId : number, trackData : object) {
+  addTrack(albumId : number, trackData : any) {
   /* Crea un track y lo agrega al album con id albumId.
   El objeto track creado debe tener (al menos):
       - una propiedad name (string),
       - una propiedad duration (number),
       - una propiedad genres (lista de strings)
   */
+    let album = this.getAlbumById(albumId);
+
+    if (album == null)
+      throw new Error(`No se pudo encontrar el album con id ${albumId}`);
+
+    const newTrack = new Track(this.getNewTrackId(), trackData.name, trackData.duration, trackData.genres);
+    album.addTrack(newTrack);
+    return newTrack;
+  }
+
+  deleteTrack(trackId: number) {
+    this.allAlbums().forEach( album =>
+      album.tracks = album.tracks.filter( track => track.id !== trackId )
+    );
+  }
+
+  private genericSearch(elementId: number, elementsArray: Array<any>) {
+    const foundedElement: any = elementsArray.find( element => element.id === elementId);
+    if (foundedElement != null)
+      console.log(JSON.stringify(foundedElement));
+    return foundedElement;
   }
 
   getArtistById(id : number): Artist {
-    const ret: Artist = this.artists.find((artist, index) => artist.id === id)
-    if (ret != null){
-      console.log("id: " + ret.id + " name: " + ret.name + " country: " +ret.country)
-    };
-    return ret;
+    return this.genericSearch(id, this.artists);
   }
 
   getAlbumById(id : number): Album {
-    return null;
+    return this.genericSearch(id, this.allAlbums());
   }
 
   getTrackById(id : number): Track{
-    return null;
+    return this.genericSearch(id, this.allTracks());
   }
 
   getPlaylistById(id : number): Playlist {
-    return null;
+    throw new Error("Not yet implemented");
   }
 
   // genres: array de generos(strings)
   // retorna: los tracks que contenga alguno de los generos en el parametro genres
   getTracksMatchingGenres(genres : Array<string>) {
-
+    throw new Error("Not yet implemented");
   }
 
   // artistName: nombre de artista(string)
   // retorna: los tracks interpredatos por el artista con nombre artistName
   getTracksMatchingArtist(artistName : string) {
-
+    throw new Error("Not yet implemented");
   }
 
 
@@ -147,18 +222,52 @@ class Artist{
   readonly id: number;
   readonly name : string;
   readonly country : string;
+  albums: Array<Album>;
+  
   constructor(id: number, name: string, country: string){
     this.id = id;
     this.name = name;
     this.country = country;
+    this.albums = [];
   }
+
+  addAlbum(album: Album) {
+    this.albums.push(album);
+  }
+
 }
 
 class Album{
+  readonly id: number;
+  readonly name: string;
+  readonly year: number;
+  tracks: Array<Track>;
+  
+  constructor(id: number, name: string, year: number) {
+    this.id = id;
+    this.name = name;
+    this.year = year;
+    this.tracks = [];
+  }
+
+  addTrack(track: Track) {
+    this.tracks.push(track);
+  }
 
 }
 
 class Track{
+  readonly id: number;
+  readonly name: string;
+  readonly duration: number;
+  readonly genres: Array<string>;
+
+  constructor(id: number, name: string, duration: number, genres: Array<string>) {
+    this.id = id;
+    this.name = name;
+    this.duration = duration;
+    this.genres = genres;
+  }
 
 }
 
