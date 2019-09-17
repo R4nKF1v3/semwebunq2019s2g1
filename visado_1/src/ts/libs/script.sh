b56a6c9599b58691bea890b9d3e4469ce52fc0fe
usuario <usuario@inforcytb05.(none)>
2019-09-17 17:34:30 +0200

    Revert "Delete script.sh"
    This reverts commit 014e19099c72f35c099f1ae8226b3c61015ccb93.

---
+++ visado_1/src/ts/libs/script.sh
@@ -0,0 +1,36 @@
+node main.js addArtist U2 Inglaterra
+node main.js addArtist ACDC Australia
+node main.js addArtist Sabina España
+
+node main.js addAlbum 1 U2_alb1 2000
+node main.js addAlbum 1 U2_alb2 2001
+
+node main.js addAlbum 2 ACDC_alb1 2003
+node main.js addAlbum 2 ACDC_alb2 2004
+
+node main.js addAlbum 3 Sabina_alb1 2000
+node main.js addAlbum 3 Sabina_alb2 2001
+
+node main.js addTrack 1 U2_track1 2000 rock
+node main.js addTrack 1 U2_track2 2000 pop
+node main.js addTrack 1 U2_track3 2000 rock
+
+node main.js addTrack 2 U2_track4 2001 rock
+node main.js addTrack 2 U2_track5 2001 pop
+node main.js addTrack 2 U2_track6 2001 rock
+
+node main.js addTrack 3 ACDC_track7 2000 metal
+node main.js addTrack 3 ACDC_track8 2000 metal
+node main.js addTrack 3 ACDC_track9 2000 rock
+
+node main.js addTrack 4 ACDC_track10 2001 rock
+node main.js addTrack 4 ACDC_track11 2001 metal
+node main.js addTrack 4 ACDC_track12 2001 rock
+
+node main.js addTrack 5 Sabina_track13 2000 melodico
+node main.js addTrack 5 Sabina_track14 2000 melodico
+node main.js addTrack 5 Sabina_track15 2000 folklore
+
+node main.js addTrack 6 Sabina_track16 2001 folklore
+node main.js addTrack 6 Sabina_track17 2001 folklore
+node main.js addTrack 6 Sabina_track18 2001 pop
