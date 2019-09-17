7ff3e313570d63ae79d43e0815818801e688bc0e
R4NK <nahumoscatelli@gmail.com>
2019-09-17 15:49:20 -0300

    Agregado User y funcionalidades respectivas al History

--- visado_1/src/ts/libs/User.ts
+++ visado_1/src/ts/libs/User.ts
@@ -1,16 +1,42 @@
+import HistoryEvent from './HistoryEvent'
+import Track from './Track';
+
 export default class User{
   readonly id: number;
   readonly name: string;
   readonly history: Array<HistoryEvent>;
 
   constructor(id: number, name: string) {
-    throw new Error("No implementado")
+    this.id = id;
+    this.name = name;
+    this.history = [];
   }
-}
 
-class HistoryEvent {
-  
-  constructor() {
-    throw new Error("No implementado")
+  listenTo(track: Track): HistoryEvent{
+    const newEvent = new HistoryEvent(track)
+    this.history.push(newEvent);
+    return newEvent;
+  }
+
+  getAllTracksListenedTo(): Array<Track>{
+    var historyReturn: Array<HistoryEvent> = [];
+    this.history.forEach(hist => {
+      if (!hist.hasSameTrack(historyReturn)){
+        historyReturn.push(hist);
+      }
+    })
+    var returnArray: Array<Track> = [];
+    historyReturn.forEach(hist => returnArray.push(hist.track));
+    return returnArray;
+  }
+
+  getTimesTrackListened(track: Track): number{
+    var times = 0;
+    this.history.forEach(event => {
+      if (event.track.id === track.id){
+        times += 1;
+      }
+    })
+    return times;
   }
 }
