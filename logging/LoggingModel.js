827a33d2fefdf0a31e13903854151f198405db5a
ferblanco75 <ferblanco@gmail.com>
2019-12-08 22:01:40 -0300

    borro metodos en controller que no se usaban

--- logging/LoggingModel.js
+++ logging/LoggingModel.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const picklify = require("picklify");
 const fs = require("fs");
 class LoggingModel {
     writeLog(cuerpo) {
@@ -12,33 +11,6 @@ class LoggingModel {
             .then(() => console.log("file created successfully with promisify!"))
             //.then() aca podria encadenar con el servicio de logly
             .catch(error => console.log(error));
-    }
-    /*prepareLogFile() {
-        if (!this.logFileExists()){
-            this.createLogFile(nombreArchivo);
-        }
-        //aca habria que cargarlo en una variable para tenerlo listo
-
-    }
-    createLogFile(nombreArchivo: any) {
-        throw new Error("Method not implemented.");
-    }
-    
-    */
-    //estas funciones son las que va a llamar el controller
-    //funcion de escribir el archivo mencionado no implementado writelog
-    //crear el archivo no implementado aun create LogFile 
-    //verificar si existe el archivo , busca en un directorio si existe 
-    //despues de creado el archivo, debo cargarlo en una variable , para poder
-    //usar fs y poder escribir los logs
-    save(filename) {
-        const serializedData = picklify.picklify(this);
-        fs.writeFileSync(filename, JSON.stringify(serializedData, null, 2));
-    }
-    static load(filename) {
-        const serializedData = fs.readFileSync(filename, { encoding: 'utf-8' });
-        const classes = [LoggingModel];
-        return picklify.unpicklify(JSON.parse(serializedData), classes);
     }
     logFileExists() {
         //let loggingModel = this.getModel();
